7b5a0ed10f97dc1a4b8873162ff8ec0d2405ef00
Weyman Wong <wongw@scotscollege.school.nz>
2024-02-29 12:37:01 +1300

    Added the load pdf buttons and text

--- app/pdf_load_widget.py
+++ app/pdf_load_widget.py
@@ -3,7 +3,7 @@ from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
 
-class PDFLoadWidget(QWidget):
+class PDFLoadWidget(QFrame):
     def __init__(self):
         super().__init__()
 
@@ -11,12 +11,52 @@ class PDFLoadWidget(QWidget):
         self.style_widgets()
 
     def create_widgets(self):
+        """
+        Initializes and lays the widgets in a vertical layout.
+
+        This widget consist of:
+            - A frame for spacing and styling
+                - A label to direct the user
+                - Horizontal Layout (for centering)
+                    - A file dialog button to select a file
+        """
+
         self.setSizePolicy(QSizePolicy.Policy.Expanding,
                            QSizePolicy.Policy.Expanding)
 
+        # Widget for spacing and styling
+        inner_widget = QFrame()
+        layout = QVBoxLayout(inner_widget)
+
+        heading = QLabel("Select a PDF file")
+        heading.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(heading)
+
+        choose_file_button = QPushButton("Choose File")
+        choose_file_button.setMaximumWidth(150)
+
+        # Widget for centering the file dialog button
+        # setAlignment is not a function for buttons
+        center_layout = QHBoxLayout()
+        center_layout.addWidget(choose_file_button)
+        layout.addLayout(center_layout)
+
+        main_layout = QVBoxLayout(self)
+
+        main_layout.addStretch(1)
+        main_layout.addWidget(inner_widget)
+        main_layout.addStretch(1)
+
     def style_widgets(self):
-        style = """ 
+        #TODO Style the widgets
+        style = """
+            QLabel, QPushButton {
+                font-size: 15px;
+            }
 
         """
 
         self.setStyleSheet(style)
+
+
+    
