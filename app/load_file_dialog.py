99552786aa0978f4ab3f67c6954efd1ad80590c9
Weyman Wong <wongw@scotscollege.school.nz>
2024-02-29 16:09:24 +1300

    Added a file dialog for loading pdfs

---
+++ app/load_file_dialog.py
@@ -0,0 +1,27 @@
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class LoadFileDialog(QFileDialog):
+    def __init__(self):
+        super().__init__()
+
+        # User can only select existing pdf files.
+        self.setFileMode(QFileDialog.FileMode.ExistingFile)
+        self.setAcceptMode(QFileDialog.AcceptOpen)
+        self.setNameFilter("PDF (*.pdf)")
+
+    def get_filename(self):
+        return self.selectedFiles()[0]
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = LoadFileDialog()
+    if (window.exec()):
+        print(window.get_filename())
+    else:
+        print("User cancelled")
+    
+    app.exec()
