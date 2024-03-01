e0cbfbd504c999c60949e85af89e9c3deb18ea91
Weyman Wong <wongw@scotscollege.school.nz>
2024-03-01 13:21:06 +1300

    Implemented the PDFScaler class into the GUI and added functionality to scale and save the desired pdf into the desired path

--- app/main_window.py
+++ app/main_window.py
@@ -1,17 +1,29 @@
+import sys
+import pathlib
+sys.path.insert(0, f"{pathlib.Path().resolve()}\\")
+
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
-
 from header import Header
 from pdf_load_widget import PDFLoadWidget
 from load_page import LoadingPage
 from pdf_viewer import PDFViewer
+from utils import PDFScaler
+
 
 
 class PDFScalerWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.create_widgets()
+        self.__pdf_path = ""
+
+    def pdf_path(self) -> str:
+        return self.__pdf_path
+
+    def set_pdf_path(self, path: str):
+        self.__pdf_path = path
 
     def create_widgets(self):
         layout = QVBoxLayout(self)
@@ -33,16 +45,21 @@ class PDFScalerWidget(QWidget):
         pdf_viewer = PDFViewer()
         self.pages.addWidget(pdf_viewer)
 
-
         # When the user selects a PDF file, the window shows the loading page
         # then loads the pdf
-        pdf_load_widget.file_selected.connect(lambda filename: self.show_loading_page())
+        pdf_load_widget.file_selected.connect(
+            lambda filename: self.show_loading_page())
         pdf_load_widget.file_selected.connect(pdf_viewer.load_pdf)
+
+        # Sets the PDF path to the file selected
+        pdf_load_widget.file_selected.connect(self.set_pdf_path)
 
         # When the pdf has been loaded, the user goes to the PDF viewer
         # Also shows the scaler
         pdf_viewer.loaded.connect(self.show_pdf_page)
         pdf_viewer.loaded.connect(lambda: self.header.set_scaler_visible(True))
+
+        self.header.scale_pdf.connect(self.start_scaling)
 
     def show_pdf_page(self):
         self.header.set_heading("Preview loaded PDF")
@@ -59,6 +76,20 @@ class PDFScalerWidget(QWidget):
     def return_header(self) -> QWidget:
         self.header = QWidget()
         layout = QHBoxLayout(header)
+
+    def scale_pdf(self, scale_factor: float, desired_path: str):
+        """
+        Takes a scale factor and the desired path to write to, uses the PDFScaler class to
+        scale and write the pdf  
+        """
+
+        scaler = PDFScaler()
+        scaler.scale_pdf(self.pdf_path(), scale_factor)
+        scaler.write_pdf(desired_path)
+
+    def start_scaling(self, scale_factor: float, desired_path: str):
+        self.show_loading_page()
+        self.scale_pdf(scale_factor / 100, desired_path)
 
 
 class PDFScalerWindow(QMainWindow):
